247f36eee1570435fc67734082c9413f5fdf9cba
tuf <tung.ns2212@gmail.com>
2024-06-05 02:04:35 +0900

    test neural network

--- aiml/from_scratch/202406_handwriting_numbers/neuralnetwork.py
+++ aiml/from_scratch/202406_handwriting_numbers/neuralnetwork.py
@@ -16,7 +16,7 @@ b_h_o = np.zeros((10,1))
 
 learn_rate = 0.01
 nr_correct = 0
-epochs = 3
+epochs = 10
 
 for epoch in range(epochs):
     for img, l in zip(images, labels):
@@ -30,4 +30,35 @@ for epoch in range(epochs):
         o = 1 / (1 + np.exp(-o_pre))
 
         # Cost, error calculation
+        e = 1 / len(o) * np.sum((o - 1) ** 2, axis=0)
+        nr_correct += int(np.argmax(o) == np.argmax(l))
 
+        # Back-propagation output -> hidden (cost function derivative)
+        delta_o = o - l
+        w_h_o += -learn_rate * delta_o @ np.transpose(h)
+        b_h_o += -learn_rate * delta_o
+        # Back-propagation hidden -> input (activation function derivative)
+        delta_h = np.transpose(w_h_o) @ delta_o * (h * (1 - h))
+        w_i_h += -learn_rate * delta_h @ np.transpose(img)
+        b_i_h += -learn_rate * delta_h
+
+    # Show accuracy for this epoch
+    print(f"Accuracy: {round((nr_correct / images.shape[0]) * 100, 2)}%")
+    nr_correct = 0
+
+# Show result
+while True:
+    index = int(input("Enter a number (0 - 59999):"))
+    img = images[index]
+    plt.imshow(img.reshape(28, 28), cmap="Greys")
+
+    img.shape += (1,)
+    # Forward propagation input -> hidden
+    h_pre = b_i_h + w_i_h @ img.reshape(784, 1)
+    h = 1 / (1 + np.exp(-h_pre))
+    # Forward propagation hidden -> output
+    o_pre = b_h_o + w_h_o @ h
+    o = 1 / (1 + np.exp(-o_pre))
+
+    plt.title(f"Subscribe if its a {o.argmax()} :)")
+    plt.show()
