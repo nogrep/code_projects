08b8929cb923c44f6cd6e112be6a9951f76c49ac
tufw <tung.ns2212@gmail.com>
2024-06-24 17:37:07 +0900

    More dynamic link

--- django/udemy/mypage/challenges/views.py
+++ django/udemy/mypage/challenges/views.py
@@ -3,6 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+monthly_challenges_by_months = {
+    "january": "Eat no meat for the entire month!",
+    "febuary": "Walk for at least 20 minutes a day.",
+    "march": "Learn Django for at least 3 hours a day.",
+    "april": "Walk for at least 20 minutes a day.",
+    "may": "Walk for at least 20 minutes a day.",
+    "june": "Walk for at least 20 minutes a day.",
+    "july": "Walk for at least 20 minutes a day.",
+    "august": "Walk for at least 20 minutes a day.",
+    "september": "Walk for at least 20 minutes a day.",
+    "october": "Walk for at least 20 minutes a day.",
+    "november": "Walk for at least 20 minutes a day.",
+    "december": "Walk for at least 20 minutes a day.",
+}
 
 def index(request):
     return HttpResponse("This works!")
@@ -12,13 +26,8 @@ def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
 def monthly_challenges(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Eat no meat for the entire month!"
-    elif month == "febuary":
-        challenge_text = "Walk for at least 20 minutes a day."
-    elif month == "march":
-        challenge_text = "Learn Django for at least 3 hours a day."
-    else:
-        challenge_text = "This month is not supported!"
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges_by_months[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponse("This month is not supported!")
