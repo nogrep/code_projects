3e775a10d3ec93cddf4178995868997f097236f5
tufw <tung.ns2212@gmail.com>
2024-06-24 17:21:13 +0900

    Dynamic path

--- django/udemy/mypage/challenges/urls.py
+++ django/udemy/mypage/challenges/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index),
-    path("febuary", views.index2)
+    path("<month>", views.monthly_challenges)
 ]
--- django/udemy/mypage/challenges/views.py
+++ django/udemy/mypage/challenges/views.py
@@ -3,8 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("This works!")
 
+
 def index2(request):
     return HttpResponse("This works too!")
+
+
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat no meat for the entire month!"
+    elif month == "febuary":
+        challenge_text = "Walk for at least 20 minutes a day."
+    elif month == "march":
+        challenge_text = "Learn Django for at least 3 hours a day."
+    else:
+        challenge_text = "This month is not supported!"
+    return HttpResponse(challenge_text)
